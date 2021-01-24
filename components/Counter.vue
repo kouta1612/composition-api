4a53088a95452f67c476e64e7e15e37b4bdf4993
kouta1612 <kouta1612world69@gmail.com>
2021-01-24 15:53:12 +0900

    add vue-compistion-api

---
+++ components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button @click="up">カウントアップ</button>
+        <p>{{ count }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        up() {
+            this.count++
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ user.firstName }}</p>
+    <p>{{ fullName }}</p>
+  </div>
+</template>
+
+<script>
+import { computed, defineComponent, reactive } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const user = reactive({
+      firstName: 'hagiwara',
+      lastName: 'kota',
+    })
+    const fullName = computed(() => `${user.firstName} ${user.lastName}`)
+    return {
+      user,
+      fullName
+    }
+  },
+})
+</script>
